0265477a1098182a76926cc7a27e1c149fd39315
justcool393 <wayhay53@gmail.com>
2015-05-13 12:34:40 -0700

    Add all approved module and fix some stuff with blocking

--- autoapprover.py
+++ autoapprover.py
@@ -1,11 +1,14 @@
-import os
+import psycopg2 as pg
 import praw
+import os
 import time
 
 # Requests' exceptions live in .exceptions and are called errors.
 from requests.exceptions import ConnectionError, HTTPError
 # Praw's exceptions live in .errors and are called exceptions.
-from praw.errors import APIException, ClientException, RateLimitExceeded
+from praw.errors import APIException, ClientException
+
+from urllib.parse import urlparse
 
 USERNAME = os.environ.get("R_USER")
 PASSWORD = os.environ.get("R_PASS")
@@ -15,6 +18,41 @@ RECOVERABLE_EXC = (ConnectionError,
                    HTTPError,
                    APIException,
                    ClientException)
+
+db_url = urlparse(os.environ.get("DATABASE_URL"))
+db = pg.connect(
+    database=db_url.path[1:],
+    user=db_url.username,
+    password=db_url.password,
+    host=db_url.hostname,
+    port=db_url.port
+)
+cur = db.cursor()
+
+
+def is_blocked(s):
+    s = s.display_name
+    cur.execute("SELECT * FROM subreddits WHERE name=%s AND mode=-1", (s,))
+    return True if cur.fetchone() else False
+
+def is_all_approved(s):
+    s = s.display_name
+    cur.execute("SELECT * FROM subreddits WHERE name=%s AND mode=1", (s,))
+    return True if cur.fetchone() else False
+
+
+class BlockedSubredditRemover:
+    def __init__(self, r):
+        self.r = r
+        self.query = "SELECT name FROM subreddits WHERE mode=-1"
+
+    def run(self):
+        for row in cur.execute(self.query):
+            s = r.get_subreddit(row[0])
+            if not s:
+                continue
+            if s.user_is_moderator:
+                s.remove_moderator(self.r.user)
 
 
 class InvitationAcceptor:
@@ -29,6 +67,8 @@ class InvitationAcceptor:
                 continue
 
             if not message.author and message.subject.startswith(INVITE_TITLE):
+                if is_blocked(message.subreddit):
+                    continue
                 self.invites.add(message.subreddit.display_name.lower())
             self.time = time.time()
         self.accept_invites()
@@ -40,6 +80,17 @@ class InvitationAcceptor:
             except praw.errors.InvalidInvite:
                 pass
         self.invites = set()
+
+
+class AllApprover:
+    def __init__(self, r):
+        self.r = r
+
+    def run(self):
+        queue = self.r.get_spam()
+        for s in queue:
+            if is_all_approved(s) and s.banned_by is None:
+                s.approve()
 
 
 class ModApprover:
@@ -54,15 +105,23 @@ class ModApprover:
                     s.approve()
 
 
+
+
 if __name__ == "__main__":
     r = praw.Reddit("Auto approves items made by moderators (/u/justcool393)")
     r.login(USERNAME, PASSWORD)
-    modules = [ModApprover(r), InvitationAcceptor(r)]
+    modules = [ModApprover(r), AllApprover(r)]
+    low_frequency_mod = [BlockedSubredditRemover(r), InvitationAcceptor(r)]
 
     while True:
+        cycle = 0
         try:
             for m in modules:
                 m.run()
+            if cycle > 9:
+                for lm in low_frequency_mod:
+                    lm.run()
+                cycle = 0
         except RECOVERABLE_EXC:
             pass
-        time.sleep(20)
+        time.sleep(10)
